d3c791a9e37be1306d4f44bcec419a0debe97234
NahuelGenchi <nahuelgenchi@gmail.com>
2022-08-21 03:14:08 -0300

    creating mixin and general scss files

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React from "react";
-import "./style.css";
+import { Header } from "./components";
+
+import "./scss/general.scss";
 
 export default function App() {
   return (
-    <div>
-      <h1>Hello StackBlitz!</h1>
-      <p>Start editing to see some magic happen :)</p>
-    </div>
+    <>
+      <Header/>
+    </>
   );
 }
---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+import "./Header.scss";
+
+const Header = () => {
+  console.log("Hello, World!");
+  return(
+    <header className="header">
+      <h1>My Website Style Guide</h1>
+    </header>
+  );
+};
+
+export default Header;
